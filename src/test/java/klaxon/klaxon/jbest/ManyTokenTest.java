0795dd3ef52fff85edf15650db4fd9ff2e453af4
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-02 09:29:31 -0400

    Lift reference values

--- src/test/java/klaxon/klaxon/jbest/ManyTokenTest.java
+++ src/test/java/klaxon/klaxon/jbest/ManyTokenTest.java
@@ -5,16 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import klaxon.klaxon.jbest.AST.Node.ArithNode;
-import klaxon.klaxon.jbest.AST.Node.LeafNode;
-import klaxon.klaxon.jbest.token.Operation;
 import klaxon.klaxon.jbest.token.Token;
 import org.junit.jupiter.api.Test;
 
 public class ManyTokenTest {
     @Test
     void many1() {
-        var input = getReferenceInput();
+        var input = Reference.getReferenceInput();
         var token = Main.makeAllTokens(input);
         assertEquals(List.of(
                 new Token.Integer(1942),
@@ -32,10 +29,10 @@ public class ManyTokenTest {
 
     @Test
     void manyTree() {
-        var input = getReferenceInput();
+        var input = Reference.getReferenceInput();
         var tokens = Main.makeAllTokens(input);
         var ast = AST.getNode(tokens.iterator()::next);
-        var reference = getReferenceAST();
+        var reference = Reference.getReferenceAST();
         var logger = Logger.getLogger("Tests");
         logger.log(Level.INFO, Util.printAST(reference));
         logger.log(Level.INFO, Util.printAST(ast));
@@ -43,37 +40,8 @@ public class ManyTokenTest {
         assertEquals(reference, ast);
     }
 
-    private static InputBlock getReferenceInput() {
-        return new InputBlock("1942 + 842 - 92 * 3 / 7");
-    }
-
-    private static ArithNode getReferenceAST() {
-        return new ArithNode(
-                new LeafNode(1942),
-                Operation.ADD,
-                new ArithNode(
-                        new LeafNode(842),
-                        Operation.SUB,
-                        new ArithNode(
-                                new LeafNode(92),
-                                Operation.MUL,
-                                new ArithNode(
-                                        new LeafNode(3),
-                                        Operation.DIV,
-                                        new LeafNode(7)
-                                )
-                        )
-                )
-        );
-    }
-
-    private static int getReferenceValue() {
-        //noinspection PointlessArithmeticExpression
-        return 1942 + (842 - (92 * (3 / 7)));
-    }
-
     @Test
     void eval() {
-        assertEquals(getReferenceValue(), Interpreter.interpret(getReferenceAST()));
+        assertEquals(Reference.getReferenceValue(), Interpreter.interpret(Reference.getReferenceAST()));
     }
 }
---
+++ src/test/java/klaxon/klaxon/jbest/Reference.java
@@ -0,0 +1,38 @@
+package klaxon.klaxon.jbest;
+
+import klaxon.klaxon.jbest.token.Operation;
+
+public class Reference {
+    static InputBlock getReferenceInput() {
+        return new InputBlock("1942 + 842 - 92 * 3 / 7");
+    }
+
+    static InputBlock getInvalidInput() {
+        return new InputBlock("1942 8392 + 842 - 92 * 3 / 7");
+    }
+
+    static AST.Node.ArithNode getReferenceAST() {
+        return new AST.Node.ArithNode(
+                new AST.Node.LeafNode(1942),
+                Operation.ADD,
+                new AST.Node.ArithNode(
+                        new AST.Node.LeafNode(842),
+                        Operation.SUB,
+                        new AST.Node.ArithNode(
+                                new AST.Node.LeafNode(92),
+                                Operation.MUL,
+                                new AST.Node.ArithNode(
+                                        new AST.Node.LeafNode(3),
+                                        Operation.DIV,
+                                        new AST.Node.LeafNode(7)
+                                )
+                        )
+                )
+        );
+    }
+
+    static int getReferenceValue() {
+        //noinspection PointlessArithmeticExpression
+        return 1942 + (842 - (92 * (3 / 7)));
+    }
+}
