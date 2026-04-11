9d46a1b3d4ea26fd106827621c88c338b8c3df23
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-11 14:21:22 -0400

    dep fastutil

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 
 val grp = "klaxon.klaxon.jbest"
 group = grp
-version = "1.0-SNAPSHOT"
+version = "0.0.0"
 
 dependencies {
+    implementation("it.unimi.dsi:fastutil:8.5.16")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
