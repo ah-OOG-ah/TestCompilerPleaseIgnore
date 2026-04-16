cde0b05d94f6a300595740b8bd3d85995d9aca5c
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-15 23:21:21 -0400

    Update Backend.java

--- src/main/java/klaxon/klaxon/jbest/codegen/Backend.java
+++ src/main/java/klaxon/klaxon/jbest/codegen/Backend.java
@@ -1,13 +1,14 @@
 package klaxon.klaxon.jbest.codegen;
 
+import jdk.jshell.spi.ExecutionControl;
 import klaxon.klaxon.jbest.AST.Node.LeafNode;
 import klaxon.klaxon.jbest.codegen.Amd64Ops.Register;
 
 public interface Backend {
-    Register load(LeafNode leaf);
+    default Register load(LeafNode leaf) { throw new UnsupportedOperationException(); }
 
-    Register add(Register right, Register left);
-    Register sub(Register right, Register left);
-    Register mul(Register right, Register left);
-    Register div(Register right, Register left);
+    default Register add(Register right, Register left) { throw new UnsupportedOperationException(); }
+    default Register sub(Register right, Register left) { throw new UnsupportedOperationException(); }
+    default Register mul(Register right, Register left) { throw new UnsupportedOperationException(); }
+    default Register div(Register right, Register left) { throw new UnsupportedOperationException(); }
 }
