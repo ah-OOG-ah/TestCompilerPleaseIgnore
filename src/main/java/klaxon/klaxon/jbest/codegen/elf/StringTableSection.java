70ab10f75de37632668854f7c941d89f8b7a3dce
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-04-18 20:24:31 -0400

    fill out sts

--- src/main/java/klaxon/klaxon/jbest/codegen/elf/StringTableSection.java
+++ src/main/java/klaxon/klaxon/jbest/codegen/elf/StringTableSection.java
@@ -1,5 +1,7 @@
 package klaxon.klaxon.jbest.codegen.elf;
 
+import static klaxon.klaxon.jbest.Util.bOfASCII;
+
 import java.util.List;
 
 public class StringTableSection extends Section {
@@ -7,11 +9,16 @@ public class StringTableSection extends Section {
 
     StringTableSection(String name, int nameIdx, long entsize) {
         super(name, nameIdx, Type.SHT_STRTAB, List.of(), entsize);
+        // The first byte must be null, so index 0 represents a null/none string
+        data.add((byte) 0);
     }
 
     /// @param s The string to add.
     /// @return The index of the added string.
+    /// @throws IllegalArgumentException if s is not ASCII
     public int addString(String s) {
-        return 0;
+        final int ret = data.size();
+        data.addAll(bOfASCII(s));
+        return ret;
     }
 }
